360ca8f5e11b10d4d3b96cdd478b3cfb75b89a28
Roman Dan <gaddyya@gmail.com>
2019-02-25 00:48:26 +0300

    feat: - deploy project

---
+++ precache-manifest.47e4539326aada0dd91a49cbf2a44407.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0ec62c3cb354549303c",
+    "url": "/static/css/main.170cad8a.chunk.css"
+  },
+  {
+    "revision": "e0ec62c3cb354549303c",
+    "url": "/static/js/main.e0ec62c3.chunk.js"
+  },
+  {
+    "revision": "6759542e3593aafbc53f",
+    "url": "/static/js/1.6759542e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2f6049cfdf1a2334daba949cf3c336b3",
+    "url": "/index.html"
+  }
+];
