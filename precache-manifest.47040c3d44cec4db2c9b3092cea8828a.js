2df568b69662f0474ce2ca4d9fa08c42b8d9531c
Roman Dan <gaddyya@gmail.com>
2019-02-25 02:26:54 +0300

    Updates

---
+++ precache-manifest.47040c3d44cec4db2c9b3092cea8828a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bffbd2f1a46be9b52b8",
+    "url": "/rss-mentor-dashboard/static/css/main.fe694979.chunk.css"
+  },
+  {
+    "revision": "8bffbd2f1a46be9b52b8",
+    "url": "/rss-mentor-dashboard/static/js/main.8bffbd2f.chunk.js"
+  },
+  {
+    "revision": "590cb28d7d3d740312c8",
+    "url": "/rss-mentor-dashboard/static/js/1.590cb28d.chunk.js"
+  },
+  {
+    "revision": "77511caa92db40fe231d",
+    "url": "/rss-mentor-dashboard/static/js/runtime~main.77511caa.js"
+  },
+  {
+    "revision": "a0e70d1ae80a6eefc246f9da8d7c8389",
+    "url": "/rss-mentor-dashboard/index.html"
+  }
+];
